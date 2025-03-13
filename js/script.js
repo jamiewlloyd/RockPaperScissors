e7b73c849c9fea1aeafadd3eb42d1db26478ea3d
Jamie Lloyd <jamielloyd.work@gmail.com>
2025-03-13 15:43:11 +0100

    Built existing code into a new function that plays a single round and updates the score

--- js/script.js
+++ js/script.js
@@ -1,21 +1,67 @@
-let humanChoice = prompt(`Please enter rock, paper or scissors`);
-console.log(humanChoice);
-
-let getComputerChoice = () => {
-   let computerChoice;
-   let randomInt = Math.floor(Math.random() * 3);
-
-   if (randomInt == 0) {
-      computerChoice = `Rock`;
-   } else if (randomInt == 1) {
-      computerChoice = `Paper`;
-   } else if (randomInt == 2) {
-      computerChoice = `Scissors`;
-   }
-
-   return computerChoice;
-}
-console.log(getComputerChoice());
-
 let humanScore = 0;
 let computerScore = 0;
+
+let playRound = (human, computer) => {
+   //declaring variables
+   let message;
+   let computerChoice;
+
+   //Getting human choice from prompt
+   let humanChoice = prompt(`Please enter rock, paper or scissors`);
+   console.log(humanChoice);
+
+   //function to get random computer choice
+   let getComputerChoice = (choice) => {
+      let randomInt = Math.floor(Math.random() * 3);
+
+      if (randomInt == 0) {
+         choice = `rock`;
+      } else if (randomInt == 1) {
+         choice = `paper`;
+      } else if (randomInt == 2) {
+         choice = `scissors`;
+      }
+
+      return choice;
+   }
+
+   //Assign computer choice to variable
+   computerChoice = getComputerChoice();
+   console.log(computerChoice);
+
+   //Conditional to decide the winner
+   if (humanChoice.toLowerCase() === computerChoice) {
+      message = `it's a tie!`;
+      human++;
+      computer++;
+   } else if (humanChoice.toLowerCase() === `rock` && computerChoice === `scissors`) {
+      message = `You Win, rock beats scissors!`;
+      human++;
+   } else if (humanChoice.toLowerCase() === `scissors` && computerChoice === `paper`) {
+      message = `You Win, scissors beats paper!`;
+      human++;
+   } else if (humanChoice.toLowerCase() === `paper` && computerChoice === `rock`) {
+      message = `You Win, paper beats rock!`;
+      human++;
+   } else if (humanChoice.toLowerCase() === `rock` && computerChoice === `paper`) {
+      message = `You lose, paper beats rock!`;
+      computer++;
+   } else if (humanChoice.toLowerCase() === `scissors` && computerChoice === `rock`) {
+      message = `You lose, rock beats scissors!`;
+      computer++;
+   } else if (humanChoice.toLowerCase() === `paper` && computerChoice === `scissors`) {
+      message = `You lose, scissors beats paper!`;
+      computer++;
+   }
+
+   alert(message);
+
+   return [human, computer];
+}
+
+let scores = playRound(humanScore, computerScore);
+humanScore = humanScore + scores[0];
+computerScore = computerScore + scores[1];
+
+console.log(humanScore);
+console.log(computerScore);
